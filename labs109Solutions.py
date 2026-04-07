a5c7e47d4aec78eee85fc0c17e9a404c8c4ec5ec
Abhishek Paul <abhishek.paul0705@gmail.com>
2026-04-07 18:20:05 -0400

    x

--- labs109Solutions.py
+++ labs109Solutions.py
@@ -1,25 +1,5 @@
-def ryerson_letter_grade(n):
-    if n < 50:
-        return "F"
-    elif n > 89:
-        return "A+"
-    elif n > 84:
-        return "A"
-    elif n > 79:
-        return "A-"
-    tens = n // 10
-    ones = n % 10
-    if ones < 3:
-        adjust = "-"
-    elif ones > 6:
-        adjust = "+"
-    else:
-        adjust = ""
-    return "DCB"[tens - 5] + adjust
-
-
 # Reverse the Rule 110
-
+# --------------------------------------------------------------------------
 def reverse_110(current):
     n = len(current)
 
@@ -49,14 +29,22 @@ def reverse_110(current):
         """Build previous state position by position"""
         # Base case: filled all positions
         if pos == n:
-            # Check if applying Rule 110 gives us current state
-            if apply_rule_110(prev) == current:
+            # FIX 3: 
+            # In our pruning logic, we check whether the neighbouring positions match the current state we want. 
+            # There is no reason to run applying rule 110 to the entire list again, as we validated all the states from 
+            # current[1] to current[n-2]; Since the states wrap around once we have reached the end of the list, the states 
+            # that we have not fully validated are the state at position 0 and the state at position n-1, as they are 
+            # dependent on what value n-1 gets. Therefore, checking if the rule_110 states are equal to the current[n-1] 
+            # and current[0] is only needed when we have reached our base case, not the entire built list, using the O(n) 
+            # cost operation of apply_rule_110
+            if rule_110[(prev[n-2], prev[n-1], prev[0])] == current[n-1] and rule_110[(prev[n-1], prev[0], prev[1])] == current[0]:
                 return prev[:]
             else:
                 return None
 
         # Try both 0 and 1 for this position
         for value in [0, 1]:
+            
             prev.append(value)
 
             # Check if this is still valid so far
@@ -67,13 +55,21 @@ def reverse_110(current):
                 check_pos = pos - 1
                 center = prev[check_pos]
 
-                # FIX 2: We don't need to use modulo n here since we are checking neighbors that are already filled. Since
-                # check_pos = pos - 1, check_pos - 1 = pos - 2 and check_pos + 1 = pos, all of these positions are already filled, 
-                # we do not need to worry about wrapping around the list. 
+                # FIX 2: 
+                # Another fix observed was reducing the overhead the algorithm caused. 
+                # The modulo in the algorithm caused unnecessary overhead since we are checking 
+                # neighbours that are already filled. check_pos = pos - 1, check_pos - 1 = pos - 2 
+                # and check_pos + 1 = pos; all of these positions are already filled, so we do not 
+                # need to worry about wrapping around the list.
                 left = prev[check_pos - 1]
-                # FIX 1: Here check_pos + 1 with never be < pos since check_pos = pos - 1 and
-                # check_pos + 1 = pos. Therefore, this condition is always false, and right is
-                # always none, therefore, we never check rule_110 and never prune invalid branches.
+                # FIX 1: 
+                # In the original implementation, check_pos + 1 will never be < pos since check_pos = pos - 1 
+                # and check_pos + 1 = pos. This causes the condition check_pos + 1 < pos to never be true 
+                # and causes right always to be none. The block of code that actually prunes the tree is 
+                # wrapped in an if statement that checks whether the right is “not” None. If this is true, 
+                # we can enter the pruning logic; if not, then we would never prune our branch. Causing the 
+                # runtime to explode horizontally. A fix for this was to simply set it right and then check expected 
+                # and prune based on those results.
                 right = prev[check_pos + 1]
 
                 expected = rule_110[(left, center, right)]
@@ -90,10 +86,11 @@ def reverse_110(current):
         return None
 
     return backtrack([], 0)
+# --------------------------------------------------------------------------
 
 
 # Post Correspondence Problem
-
+# --------------------------------------------------------------------------
 def post_correspondence_problem(first, second, lo, hi):
 
     def backtrack(first_str, second_str):
@@ -112,10 +109,13 @@ def post_correspondence_problem(first, second, lo, hi):
             new_second = second_str + second[i]
 
             # FIX 1:
-            # The first fix that was made was to improve the pruning. In the post_correspondence_problem implementation,
-            # no matter if the addition of elements from the first and second lists would make the strings never be compatible.
-            # If we check if the new first string or the new second string is in the prefix of the other, we know these strings
-            # still have a chance at being equivalent later in the recursion. If not, then they have no chance.
+            # The first fix that was made was to improve the pruning. In the original implementation, there was no pruning involved, 
+            # so the implementation was simply brute force until we achieved equal strings that were within the length of high and 
+            # low constraints. A pruning fix that would greatly reduce the branches of the subtrees would be to confirm if the 2 string 
+            # prefixes matched each other. In other words, string A either starts with string B or string B starts with string A. 
+            # This would greatly reduce the branching, as when we confirm that neither of the 2 strings starts with the other, then 
+            # this can never be a valid solution, so we would end traversing this branch early. Overall, we would significantly decrease 
+            # the size of the tree that is being traversed with every prune, reducing the overall number of recursive calls. 
             if (new_second.startswith(new_first) or new_first.startswith(new_second)):
                 if backtrack(new_first, new_second):
                     return True
@@ -125,10 +125,11 @@ def post_correspondence_problem(first, second, lo, hi):
         return False
 
     return backtrack("", "")
+# --------------------------------------------------------------------------
 
 
 # bandwith
-
+# --------------------------------------------------------------------------
 def bandwidth(edges):
     n = len(edges)
 
@@ -147,28 +148,38 @@ def bandwidth(edges):
 
             # Choose next node to number (could be optimized)
 
-            # FIX 1: In the default implementation, the next node we assign is just the sequential nodes starting from 0;
-            # this implementation is not as fast as picking a node that is already limited to a small number of options,
-            # meaning we are reducing our branching as well as the width of the search tree. To implement this, we would
-            # be picking the node with the most neighbours that are already assigned at each recursive step. This would give
-            # us the node with the least amount of options and branching. Overall, this would greatly reduce the branching
-            # and speed of the algorithm.
+            # FIX 1: 
+            # In the default implementation, the next node we assign a value to is just the sequential nodes starting from 0; 
+            # this is not as optimal as picking a node with a small number of options, which reduces our branching and the 
+            # width of the search tree. This is much like picking the most constrained node to assign. To implement this, 
+            # we would pick the node with the most neighbours already assigned at each recursive step. This would give us the
+            # node with the most constraints with more restrictions and would allow invalid assignments to be detected earlier, 
+            # reducing branches. Overall, this would greatly reduce the branching and improve the algorithm's speed.
             node = -1
-            maxEdge = float("-inf")
+            # maxEdge = float("-inf")
+            min_num_allowed_values = float("inf")
 
             for i in range(n):
 
                 if numbering[i] == -1:
 
-                    numEdge = 0
+                    # numEdge = 0
 
-                    for neighbor in edges[i]:
-                        if numbering[neighbor] != -1:
-                            numEdge += 1
+                    # for neighbor in edges[i]:
+                    #     if numbering[neighbor] != -1:
+                    #         numEdge += 1
 
-                    if numEdge > maxEdge:
-                        maxEdge = numEdge
+                    # if numEdge > maxEdge:
+                    #     maxEdge = numEdge
+                    #     node = i
+
+                    # FIX 3:
+                    num_allowed_values = (node_allowed_bandwidth[i][1] - node_allowed_bandwidth[i][0]) + 1
+
+                    if num_allowed_values < min_num_allowed_values:
+                        min_num_allowed_values = num_allowed_values
                         node = i
+
 
             # Try each available number
             for num in range(node_allowed_bandwidth[node][0], node_allowed_bandwidth[node][1] + 1):
@@ -194,10 +205,13 @@ def bandwidth(edges):
 
                 changes = []
                 
-                # FIX 2: We can have a allowed range of numbers for each node after we select a node to be place we update the neibourse allowed ranges so that they can still be assigned a valid number.
-                # If this interval becomes empty, as the ranges of high and low are not valid (high > low) then we can prune that branch. Addtionally using this new allowed range, we can only try these numbers
-                # for the current node we are assigning, greatly reducing the looping in this function. With this implementation, we are also storing our changes so that we can backtrack when we need to
-                # prune this branch. 
+                # FIX 2: 
+                # We can have an allowed range of [0…n-1] for each node. After we select a node to be placed, we update the neighbours' 
+                # allowed ranges so that they can still be assigned a valid number. If this interval becomes empty, as the ranges of 
+                # high and low are not valid (low > high), then we can prune that branch. This is like constraint propagation, where 
+                # we confirm that all remaining neighbours of our node can be a value based on the constraints. Additionally, using this 
+                # new allowed range, we can try these numbers for the current node we are assigning, greatly reducing the looping in this 
+                # function. Although there is now a list that tracks changes, increasing space complexity
                 for neighbor in edges[node]:
                     if numbering[neighbor] == -1:
 
@@ -239,10 +253,11 @@ def bandwidth(edges):
             return bw
 
     return n - 1
+# --------------------------------------------------------------------------
 
 
 # Stepping stones
-
+# --------------------------------------------------------------------------
 def stepping_stones(n, ones):
     # Initialize board with stones
     board = {}  # (row, col) -> stone number
@@ -326,10 +341,11 @@ def stepping_stones(n, ones):
         return best
 
     return backtrack(2)
+# --------------------------------------------------------------------------
 
 
 # unity_partition
-
+# --------------------------------------------------------------------------
 def unity_partition(n):
     from fractions import Fraction
     
@@ -341,25 +357,31 @@ def unity_partition(n):
         # Dead ends
         if target_sum <= 0 or target_frac <= 0:
             return None
+            
         
         # Try each possible next number
-        for num in range(start, target_sum):
-
+        for num in range(start, target_sum+1):
+            
             reciprocal = Fraction(1, num)
             
             # Skip if reciprocal is too large
             if reciprocal > target_frac:
                 continue
 
-            if target_sum - num < 0:
-                continue
-
             # FIX 1: We are checking that in the best case if we take remaning recipriocal sums before we exceed our remaining budget for sum, 
             # we can still reach our target fraction. If our max fraction is less that our target fraction we know in the best case, taking
             # all the allowed remaining numbers, we won't reach our target fraction and this path will not lead to a solution.
             remaining_sum = target_sum - num
+            remaining_fraction = target_frac - reciprocal
             temp = num+1
             max_fraction = 0 
+
+            # FIX 2: Here we check that in the best case if we take the smallest reciprocal possible to reach our target remaining recipricol, 
+            # will we exceed the remaining fraction. That then we know that the paths that come from this point will not lead to a solution and 
+            # we can prune this path. As the smalled reciprocol sum is already greater than the remaining fraction, we know that any other recipricol 
+            # sum will also be greater than the remaining fraction and thus we can prune this path.
+            if(remaining_sum > 0 and Fraction(1, remaining_sum) > remaining_fraction):
+                continue
 
             while True:
                 if(remaining_sum - temp < 0):
@@ -369,12 +391,12 @@ def unity_partition(n):
                     max_fraction += 1 / temp
                     temp += 1
 
-            if(max_fraction < target_frac - reciprocal):
-                continue
+            if(max_fraction < remaining_fraction):
+                return None
 
             # Try adding this number
             current_list.append(num)
-            result = backtrack(target_sum - num, target_frac - reciprocal, current_list, num + 1)
+            result = backtrack(target_sum - num, remaining_fraction, current_list, num + 1)
             
             if result is not None:
                 return result
@@ -385,3 +407,4 @@ def unity_partition(n):
     
     result = backtrack(n, Fraction(1, 1), [], 2)
     return result if result else []
+# --------------------------------------------------------------------------
